a9afebf370070b757222c42e108f890b3c5d91a5
Mrjunaid555 <96606726+Mrjunaid555@users.noreply.github.com>
2022-06-14 11:45:30 -0700

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop
+{    
+	public static void main(String[] args) 
+{    
+    	int k=1;    
+    	do
+	{    
+        System.out.println(i);    
+    	k++;    
+    	}
+	while(k<=10);    
+}    
+}  
